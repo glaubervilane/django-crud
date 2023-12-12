9e14c74a35f3eca6e391647fc2691e28ecc16a30
yourusernameglaubervilanegit config --global user.email glauberfmarques@gmail.comgit config --global user.name yourusernameglaubervilane <glauberfmarques@gmail.com>
2023-12-12 07:57:48 -0700

    Implement create, read and update in CRUD operations

--- core/views.py
+++ core/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
 
 def home(request):
   people = Person.objects.all()
   return render(request, "index.html", {"people": people})
+
+def save(request):
+  vname = request.POST.get("name")
+  Person.objects.create(name=vname)
+  people = Person.objects.all()
+  return render(request, "index.html", {"people": people})
+
+def edit(request, id):
+  person = Person.objects.get(id=id)
+  return render(request, "update.html", {"person": person})
+
+def update(request, id):
+  vname = request.POST.get("name")
+  person = Person.objects.get(id=id)
+  person.name = vname
+  person.save()
+  return redirect(home)
